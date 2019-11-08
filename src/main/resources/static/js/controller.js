cddeaaa1fd3979cb90242dd602175df16fa15a34
Aldo Flores Aparicio <afloresa@ultrasist.com.mx>
2019-11-07 18:54:55 -0600

    Lecciones 5 y 6 de codigo facilito. Modulos, inyeccion de dependencias, minificado, sistema de comentarios, ng-repeat, ng-click

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,4 +1,34 @@
 var app = angular.module("MyFirstApp", []);
-app.controller("FirstController", function($scope) {
-	$scope.nombre = "Aldo";
-});
+/*
+ * Como buena practica en produccion se comprimen los archivos JS 
+ * esto se llama minificado, esto tambien cambia el codigo por 
+ * palabras mas pequeñas.
+ * Se cambio la declaracion de la funcion anonima por un arreglo 
+ * con la lista de las dependencias seguido de la funcion anomina
+ * con un alias de las dependencias.
+ * Esto se hace para que al momento de minificar no se le cambie el 
+ * nombre a las variables de angular
+ * El minificado no cambia nombre a cadenas
+ * */
+app.controller("FirstController", ["$scope",function(scope) {
+	scope.nombre = "Aldo";
+	scope.nuevoComentario={};
+	scope.comentarioBack={};
+	scope.comentarios=[
+		{
+			comentario: "Creo que ests pagina esta sencilla pero funcional",
+			username: "Juanito"
+		},
+		{
+			comentario: "Me gusto la pagina funciona bien",
+			username: "Pedro"			
+		}	
+	];
+	
+	scope.agregarComentario = function(){
+		scope.comentarioBack = scope.nuevoComentario; 
+		scope.comentarios.push(scope.comentarioBack);
+		scope.nuevoComentario={};
+	}
+	
+}]);
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -4,12 +4,25 @@
 <head>
 	<meta charset="utf-8">
 	<title>Welcome</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script>
-	<script type="text/javascript" src="js/controller.js"></script>
 </head>
 <body ng-controller="FirstController">
+	<label>Ingrese un nuevo comentario:</label>
+	<input type="text" ng-model="nuevoComentario.comentario">
+	<br>
+	<label>Ingrese su nombre</label>
+	<input type="text" ng-model="nuevoComentario.username">
+	<br>
+	<button ng-click="agregarComentario()">Agregar Comentario</button>
+	
+	<h3>Comentarios</h3>
+ 	<ul>
+ 		<li ng-repeat="comentario in comentarios">
+			{{comentario.comentario}} - <strong>{{comentario.username}}</strong> 			
+ 		</li>
+ 	</ul>
 
-	{{"Hola "+ nombre}}
-
+	<!-- Las dependencias se JS se cargan al final para que no retrase la carga de la pagina -->
+ 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script>
+	<script type="text/javascript" src="js/controller.js"></script> 	
 </body>
 </html>
